676235d0c35355ab957aa76cc3f4ce3da0b3fa1c
siddartharaothota <siddartharao8@gmail.com>
2026-02-22 13:00:04 +0530

    small changes

--- serverMongodb.js
+++ serverMongodb.js
@@ -7,7 +7,6 @@ const cors = require("cors");
 const os = require("os");
 
 const app = express();
-const PORT = 8000;
 
 app.use(cors());
 app.use(express.json());
@@ -16,6 +15,8 @@ app.use(express.urlencoded({ extended: true }));
 // ==============================
 // MongoDB Connection
 // ==============================
+
+const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.MONGO_URI);
 
